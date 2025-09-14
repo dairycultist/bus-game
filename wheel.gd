56ab85d7e6efb1ccfb31608a14b0375c33be0903
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-09-14 18:48:12 -0400

    Refactor

--- wheel.gd
+++ wheel.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 # the wheel node indicates the positiona at zero compression (aka the lowest point).
 
+# -z direction is forward
+
 @export var max_compression_distance: float = 1.0
 @export var stiffness: float = 50.0
 @export var powered: bool = true
@@ -23,12 +25,13 @@ func _process(delta: float) -> void:
 	# the suspension is compressed and we should apply a suspension force to
 	# the chassis
 	var compression_distance := 0.
+	var compression_amount   := 0.
 	
 	if (ray_result):
 		
 		compression_distance = max_compression_distance - (max_compression_position - ray_result.position).length()
+		compression_amount = compression_distance / max_compression_distance
 		
-		var compression_amount = compression_distance / max_compression_distance
 		chassis.apply_force(compression_amount * chassis_up * stiffness, global_position + chassis_up * max_compression_distance - chassis.global_position)
 	
 	# visibly push our mesh up during compression
