de3fc6b79273feca6ff1bd44eecb44804ac0625b
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-09-14 18:29:01 -0400

    Wheel suspension but with raycasting

--- wheel.gd
+++ wheel.gd
@@ -1,21 +1,29 @@
-extends RigidBody3D
+extends Node3D
 
 # goal: maintain the initial relative offset from the chassis (parent) by applying
 #       dampened forces to the chassis
 
-var target_position: Vector3
+# the wheel node starts at zero compression (aka the lowest point). it raycasts
+# from the point of max_compression down; if it hits something before the point
+# of zero compression, it means it's compressed
 
-func _ready() -> void:
-	target_position = position
-	
-	axis_lock_angular_x = true
-	axis_lock_angular_y = true
-	axis_lock_angular_z = true
+@export var max_compression: float = 1.0
 
 func _process(delta: float) -> void:
 	
 	var chassis := get_parent_node_3d()
+	var compression := 0.
+	var chassis_up := global_transform.basis.y
 	
-	chassis.apply_force((position - target_position) * 100., target_position)
+	var max_compression_position = global_position + chassis_up * max_compression
 	
-	rotation = chassis.rotation
+	var ray_result = get_world_3d().direct_space_state.intersect_ray(PhysicsRayQueryParameters3D.create(
+		max_compression_position,
+		global_position
+	))
+	
+	if (ray_result):
+		compression = max_compression - (max_compression_position - ray_result.position).length()
+		chassis.apply_force(compression * chassis_up * 100., global_position + chassis_up * max_compression - chassis.global_position)
+	
+	$Mesh.position.y = compression + 0.2
