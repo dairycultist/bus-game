21564646ec2329a89d93c9769a64fcecac135a44
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-09-14 18:42:52 -0400

    Improve suspension math

--- wheel.gd
+++ wheel.gd
@@ -1,29 +1,35 @@
 extends Node3D
 
-# goal: maintain the initial relative offset from the chassis (parent) by applying
-#       dampened forces to the chassis
+# the wheel node indicates the positiona at zero compression (aka the lowest point).
 
-# the wheel node starts at zero compression (aka the lowest point). it raycasts
-# from the point of max_compression down; if it hits something before the point
-# of zero compression, it means it's compressed
-
-@export var max_compression: float = 1.0
+@export var max_compression_distance: float = 1.0
+@export var stiffness: float = 50.0
+@export var powered: bool = true
 
 func _process(delta: float) -> void:
 	
 	var chassis := get_parent_node_3d()
-	var compression := 0.
 	var chassis_up := global_transform.basis.y
 	
-	var max_compression_position = global_position + chassis_up * max_compression
+	# raycast from the point of max_compression_distance down
+	var max_compression_position = global_position + chassis_up * max_compression_distance
 	
 	var ray_result = get_world_3d().direct_space_state.intersect_ray(PhysicsRayQueryParameters3D.create(
 		max_compression_position,
 		global_position
 	))
 	
-	if (ray_result):
-		compression = max_compression - (max_compression_position - ray_result.position).length()
-		chassis.apply_force(compression * chassis_up * 100., global_position + chassis_up * max_compression - chassis.global_position)
+	# if the ray hits something before the point of zero compression, it means
+	# the suspension is compressed and we should apply a suspension force to
+	# the chassis
+	var compression_distance := 0.
 	
-	$Mesh.position.y = compression + 0.2
+	if (ray_result):
+		
+		compression_distance = max_compression_distance - (max_compression_position - ray_result.position).length()
+		
+		var compression_amount = compression_distance / max_compression_distance
+		chassis.apply_force(compression_amount * chassis_up * stiffness, global_position + chassis_up * max_compression_distance - chassis.global_position)
+	
+	# visibly push our mesh up during compression
+	$Mesh.position.y = compression_distance + 0.2
