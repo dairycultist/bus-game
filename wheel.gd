c32814d36be9a1b286b282a9c7d55f2654c7ea61
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-09-14 19:08:35 -0400

    Suspension driving and damping

--- wheel.gd
+++ wheel.gd
@@ -2,16 +2,19 @@ extends Node3D
 
 # the wheel node indicates the positiona at zero compression (aka the lowest point).
 
-# -z direction is forward
-
+# values that work good enough for a 100kg car with 0 linear damp and 5 angular damp
 @export var max_compression_distance: float = 1.0
-@export var stiffness: float = 50.0
+@export var stiffness: float = 1500.0
+@export var dampening: float = 0.1
 @export var powered: bool = true
+@export var drive_force: float = 50.0
 
 func _process(delta: float) -> void:
 	
 	var chassis := get_parent_node_3d()
 	var chassis_up := global_transform.basis.y
+	var chassis_forward := -global_transform.basis.z # -z direction is forward
+	var chassis_force_position := global_position + chassis_up * max_compression_distance - chassis.global_position
 	
 	# raycast from the point of max_compression_distance down
 	var max_compression_position = global_position + chassis_up * max_compression_distance
@@ -32,7 +35,18 @@ func _process(delta: float) -> void:
 		compression_distance = max_compression_distance - (max_compression_position - ray_result.position).length()
 		compression_amount = compression_distance / max_compression_distance
 		
-		chassis.apply_force(compression_amount * chassis_up * stiffness, global_position + chassis_up * max_compression_distance - chassis.global_position)
+		# apply spring force
+		chassis.apply_force(compression_amount * chassis_up * stiffness, chassis_force_position)
+		
+		# apply dampening force opposite to current up-down speed
+		var updown_speed = chassis.linear_velocity.dot(chassis_up)
+		chassis.apply_force(dampening * updown_speed * -chassis_up * stiffness, chassis_force_position)
 	
 	# visibly push our mesh up during compression
 	$Mesh.position.y = compression_distance + 0.2
+	
+	# input/powering
+	if (powered and compression_amount > 0.02):
+		
+		if (Input.is_action_pressed("ui_up")):
+			chassis.apply_force(chassis_forward * drive_force, chassis_force_position)
