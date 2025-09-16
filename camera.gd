e4c745cd483baa27978add9d522e4b2555d5c248
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-09-15 23:02:28 -0400

    Camera following

---
+++ camera.gd
@@ -0,0 +1,12 @@
+extends Camera3D
+
+@export var target: Node3D
+@export var follow_speed: float = 10.0
+
+func _ready() -> void:
+	global_position = target.global_position
+	global_rotation = target.global_rotation
+
+func _process(delta: float) -> void:
+	global_position = lerp(global_position, target.global_position, delta * follow_speed)
+	global_basis = Basis(Quaternion(global_basis).slerp(Quaternion(target.global_basis), delta * follow_speed))
