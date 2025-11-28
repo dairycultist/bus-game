292701a0816f06daa70cedf2e9a2a98771cb1caa
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-11-27 21:09:55 -0500

    Create track mesh; Test more traditional racecar parameters

--- camera.gd
+++ camera.gd
@@ -3,6 +3,8 @@ extends Camera3D
 @export var target: Node3D
 @export var follow_speed: float = 10.0
 
+# TODO camera should always stay above bus looking down, even if it leans or tips over
+
 func _ready() -> void:
 	global_position = target.global_position
 	global_rotation = target.global_rotation
