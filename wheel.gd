12890a28a9ffb1d5033cf9f041505c2f0a203446
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-12-05 02:12:08 -0500

    Cut the fat (and some features for now)

--- wheel.gd
+++ wheel.gd
@@ -3,12 +3,10 @@ extends Node3D
 # the wheel node indicates the positions at zero compression (aka the lowest point)
 # it should be the child of a rigidbody representing the car's chassis
 
-# default values work good enough for a 100kg car
-
 # https://sketchfab.com/3d-models/cartoon-car-09431a8df4344a84aa83a4095bef10fc
 
 @export_category("Suspension")
-@export var max_compression_distance: float = 1.0 # ensure this point is located within the parent car's collider to prevent the tire "falling through" level collision
+@export var max_compression_distance: float = 1.0
 @export var stiffness: float = 1500.0
 @export var dampening: float = 150.0
 
@@ -29,14 +27,16 @@ enum SteerType {
 	RightTurnsLeft
 }
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	
-	var chassis                        := get_parent_node_3d()
-	var chassis_up                     :=  chassis.global_transform.basis.y
-	var chassis_forward                := -chassis.global_transform.basis.z # -z direction is forward
-	var chassis_to_suspension_position := global_position + chassis_up * max_compression_distance - chassis.global_position
+	_turn_wheels(delta)
+	_compress_suspension()
 	
-	# turn wheel in direction of inputed steer
+	# get relative wheel contact position for applying forces to the chassis
+	#var chassis_to_wheel_contact_position = global_position + compression_distance * chassis_up - chassis.global_position
+
+func _turn_wheels(delta):
+	
 	var steer_rotation := 0.0
 	
 	if (steered == SteerType.RightTurnsRight):
@@ -55,17 +55,27 @@ func _process(_delta: float) -> void:
 		if (Input.is_action_pressed("ui_right")):
 			steer_rotation += deg_to_rad(max_turn_angle)
 	
-	rotation.y = steer_rotation
+	rotation.y = lerp(rotation.y, steer_rotation, delta * 10)
+
+func _compress_suspension() -> float: # returns amount of compression, [0..1]
 	
-	# raycast from the point of max_compression_distance down,
-	# ignoring the car's collider
-	var max_compression_position = global_position + chassis_up * max_compression_distance
+	# TODO completely remove max_compression_distance
+	# have suspension mount position be determined by a parent node
+	# have spring force be based on compression distance, not amount
 	
+	var chassis                   := get_parent_node_3d()
+	var chassis_up                := chassis.global_transform.basis.y
+	var suspension_mount_position := global_position + chassis_up * max_compression_distance - chassis.global_position
+	
+	var max_compression_global_position := global_position + chassis_up * max_compression_distance
+	
+	# raycast somewhere above max_compression_position down (to prevent
+	# clipping through the ground), ignoring the chassis's collider
 	var query = PhysicsRayQueryParameters3D.create(
-		max_compression_position,
+		max_compression_global_position + chassis_up * 0.5,
 		global_position
 	)
-	query.exclude = [get_parent_node_3d()]
+	query.exclude = [chassis]
 	
 	var ray_result = get_world_3d().direct_space_state.intersect_ray(query)
 	
@@ -73,43 +83,24 @@ func _process(_delta: float) -> void:
 	# the suspension is compressed, the wheel is grounded, and we should apply a
 	# relevant forces to the chassis
 	var compression_distance := 0.0
+	var compression_amount := 0.0
 	
 	if (ray_result):
 		
-		compression_distance   = max_compression_distance - (max_compression_position - ray_result.position).length()
-		var compression_amount = compression_distance / max_compression_distance
+		compression_distance = (global_position - ray_result.position).length()
+		compression_amount = compression_distance / max_compression_distance
 		
-		# apply spring force at the chassis_to_suspension_position based on compression_amount
-		chassis.apply_force(compression_amount * chassis_up * stiffness, chassis_to_suspension_position)
+		# apply spring force at the suspension_mount_position based on compression_amount
+		chassis.apply_force(compression_amount * chassis_up * stiffness, suspension_mount_position)
 		
-		# apply dampening force at the chassis_to_suspension_position opposite
-		# to the vertical velocity at the chassis_to_suspension_position
-		var velocity_at_position = chassis.linear_velocity + chassis.angular_velocity.cross(chassis_to_suspension_position)
+		# apply dampening force at the suspension_mount_position opposite
+		# to the vertical velocity at the suspension_mount_position
+		var velocity_at_position = chassis.linear_velocity + chassis.angular_velocity.cross(suspension_mount_position)
 		var vertical_velocity_at_position = velocity_at_position.dot(chassis_up)
 		
-		chassis.apply_force(vertical_velocity_at_position * -chassis_up * dampening, chassis_to_suspension_position)
-		
-		# get relative wheel contact position for applying forces to the chassis
-		var chassis_to_wheel_contact_position = global_position + compression_distance * chassis_up - chassis.global_position
-		
-		# apply force opposite to side-to-side wheel slip at chassis_to_wheel_contact_position
-		# (angle of wheel is already accounted for in its transform)
-		var sidetoside_velocity_at_position = velocity_at_position.dot(global_transform.basis.x)
-		
-		chassis.apply_force(sidetoside_velocity_at_position * -global_transform.basis.x * antislip, chassis_to_wheel_contact_position)
-		
-		# apply drive_force at the chassis_to_wheel_contact_position proportional
-		# to compression distance (a barely grounded tire barely applies force! prevents bouncing)
-		# TODO should take into account current speed? (for applying force AND mesh rotation around x)
-		if (powered):
-			
-			var wheel_forward := -global_transform.basis.z
-			
-			if (Input.is_action_pressed("ui_up")):
-				chassis.apply_force(wheel_forward * drive_force * compression_distance / max_compression_distance, chassis_to_wheel_contact_position)
-			
-			if (Input.is_action_pressed("ui_down")):
-				chassis.apply_force(-wheel_forward * drive_force * compression_distance / max_compression_distance, chassis_to_wheel_contact_position)
+		chassis.apply_force(vertical_velocity_at_position * -chassis_up * dampening, suspension_mount_position)
 	
 	# visibly push our mesh up during compression
 	$Mesh.position.y = compression_distance + 0.2
+	
+	return compression_amount
