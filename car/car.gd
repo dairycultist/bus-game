a2735a74cac9da86fea17500e9ecfe2a72024208
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-02-18 22:15:36 -0500

    Parameterize car controls

--- car/car.gd
+++ car/car.gd
@@ -1,24 +1,29 @@
 extends RigidBody3D
 
+@export var drive: float = 5000.0
+@export var steer: float = 2000.0
+@export var grip:  float = 300.0
+
 func _process(_delta: float) -> void:
 	
 	var input = Input.get_vector("ui_left", "ui_right", "ui_down", "ui_up")
 	
-	apply_force(-global_basis.z * 5000.0 * input.y)
+	apply_force(-global_basis.z * drive * input.y)
 	
-	apply_torque(-global_basis.y * 2000.0 * input.x)
+	apply_torque(-global_basis.y * steer * input.x)
 	
 	# oppose motion at the front and back of car
 	_oppose_at(Vector3.FORWARD)
 	_oppose_at(Vector3.BACK)
 	
 	# car lean
-	$Mesh.rotation.z = global_basis.x.dot(linear_velocity) * abs(global_basis.x.dot(linear_velocity)) * 0.003
+	var sidevel := global_basis.x.dot(linear_velocity)
+	$Mesh.rotation.z = sidevel * abs(sidevel) * 0.001
 
 func _oppose_at(pos: Vector3):
 	
 	var velocity_at_position := linear_velocity + angular_velocity.cross(global_basis * pos)
 	
-	var antislip = global_basis.x.dot(velocity_at_position) * 300.0
+	var antislip = global_basis.x.dot(velocity_at_position) * grip
 	
 	apply_force(-global_basis.x * antislip, global_basis * pos)
