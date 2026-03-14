ce6b4113d4c039808116c24c5d22ba594d4fbaa1
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-03-14 14:15:03 -0400

    Clean up

--- car/wheel.gd
+++ car/wheel.gd
@@ -4,3 +4,5 @@ func _process(delta: float) -> void:
 	
 	if $ContactRay.is_colliding():
 		$WheelContact.global_position = $ContactRay.get_collision_point()
+	else:
+		$WheelContact.global_position = $ContactRay.to_global($ContactRay.target_position)
