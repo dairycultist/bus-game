0b4fd022898ced6c32bc9cab97349f46149d41bc
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-03-14 14:17:49 -0400

    Prepare for wheel physics

--- car/wheel.gd
+++ car/wheel.gd
@@ -1,5 +1,12 @@
 extends Node3D
 
+@export var flip_mesh: bool
+
+func _ready() -> void:
+	
+	if flip_mesh:
+		$WheelContact/Mesh.scale.x *= -1.0
+
 func _process(delta: float) -> void:
 	
 	if $ContactRay.is_colliding():
