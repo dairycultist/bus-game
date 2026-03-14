0900d67a3480ce0f2488bc8f5e0e58ae546e7a40
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-03-14 18:19:15 -0400

    Prepare for realistic, wheel-rotation based drive

--- car/wheel.gd
+++ car/wheel.gd
@@ -6,17 +6,22 @@ extends Node3D
 
 @onready var body: RigidBody3D = get_parent()
 
+var wheel_radius: float
+var angular_speed: float
+
 func _ready() -> void:
 	
 	if flip_mesh:
 		$WheelContact/Mesh.scale.x *= -1.0
+	
+	wheel_radius = $WheelContact/Mesh.position.y
 
 func _process(_delta: float) -> void:
 	
-	if $ContactRay.is_colliding():
-		$WheelContact.global_position = $ContactRay.get_collision_point()
-	else:
-		$WheelContact.global_position = $ContactRay.to_global($ContactRay.target_position)
+	$WheelContact.global_position =\
+		$ContactRay.get_collision_point()\
+		if $ContactRay.is_colliding()\
+		else $ContactRay.to_global($ContactRay.target_position)
 
 func _physics_process(_delta: float) -> void:
 	
@@ -29,6 +34,18 @@ func _physics_process(_delta: float) -> void:
 	body.apply_force(global_basis.y * compression_fac * stiffness, global_position)
 	
 	# resist velocity
-	var local_velocity: Vector3 = body.linear_velocity + body.angular_velocity.cross(global_position - body.global_position)
+	var velocity_at_origin: Vector3 = body.linear_velocity + body.angular_velocity.cross(global_position - body.global_position)
 	
-	body.apply_force(-local_velocity * dampening, global_position)
+	body.apply_force(-velocity_at_origin * dampening, global_position)
+	
+	# velocity at the point of contact agnostic to the rotation of the wheel
+	var body_velocity_at_contact: Vector3 = body.linear_velocity + body.angular_velocity.cross($WheelContact.global_position - body.global_position)
+	
+	# velocity of the wheel at the point of contact agnostic to the car body
+	var wheel_velocity_at_contact: Vector3 = angular_speed * wheel_radius * -$WheelContact.global_basis.z
+	
+	# apply force to coax wheel into not slipping
+	# (when wheel is spinning, this will both account for drive and antislip)
+	# (might also want to be able to have velocity_at_contact influence angular_speed, idk)
+	
+	# when there is no slip, body_velocity_at_contact = -wheel_velocity_at_contact
