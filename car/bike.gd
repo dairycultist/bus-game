3cd00a3a5e53f37092ea8d08d36f50f7f894f335
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-03-16 00:59:12 -0400

    Springy wheels 2

--- car/bike.gd
+++ car/bike.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@export var stiffness: float = 3000.0
+@export var stiffness: float = 8000.0
 
 func _input(event):
 	
@@ -11,18 +11,19 @@ func _input(event):
 
 func _physics_process(_delta: float) -> void:
 	
+	#$FrontWheel.global_position = $Chassis/FrontSuspension.to_global(
+		#$Chassis/FrontSuspension.curve.get_closest_point(
+			#$Chassis/FrontSuspension.to_local($FrontWheel.global_position)
+		#)
+	#)
+	
+	var suspension_mount: Vector3 = $Chassis/FrontSuspension.to_global($Chassis/FrontSuspension.curve.get_point_position(1))
+	
+	var compress: Vector3 = $FrontWheel.global_position - $Chassis/FrontSuspension.to_global($Chassis/FrontSuspension.curve.get_point_position(0))
+	
+	$Chassis.apply_force(compress * stiffness, suspension_mount - $Chassis.global_position)
+	$FrontWheel.apply_force(-compress * stiffness)
+	
 	# wheel is a separate physical object from the bike itself
 	# normal/frictional forces act on the wheel, which applies normal/spring forces on the bike
 	# free body diagram
-	$FrontWheel.global_position = $Chassis/FrontSuspension.to_global(
-		$Chassis/FrontSuspension.curve.get_closest_point(
-			$Chassis/FrontSuspension.to_local($FrontWheel.global_position)
-		)
-	)
-	
-	var compression_dist: float = ($FrontWheel.global_position - $Chassis/FrontSuspension.to_global($Chassis/FrontSuspension.curve.get_point_position(0))).length()
-	
-	var suspension_dir: Vector3 = $Chassis/FrontSuspension.to_global($Chassis/FrontSuspension.curve.get_point_position(1) - $Chassis/FrontSuspension.curve.get_point_position(0)).normalized()
-	var suspension_mount: Vector3 = $Chassis/FrontSuspension.to_global($Chassis/FrontSuspension.curve.get_point_position(1))
-	
-	$Chassis.apply_force(suspension_dir * compression_dist * stiffness, suspension_mount - $Chassis.global_position)
