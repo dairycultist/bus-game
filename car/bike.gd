ba6e22765e30d4d2ccd0f7a0b3a942cc645d2cb3
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-03-16 02:03:03 -0400

    Physics is hard PIVOT YAY

--- car/bike.gd
+++ car/bike.gd
@@ -1,35 +1,31 @@
-extends Node3D
+extends CharacterBody3D
 
-@export var stiffness: float = 8000.0
-@export var dampening: float = 100.0
+# visual
+@export var pitch_intensity: float = 0.07
+@export var roll_intensity: float = 0.5
+@export var wheel_mesh_radius: float = 1.2
 
 func _input(event):
 	
 	if event.as_text() == "1":
-		$Chassis/Camera1.make_current()
+		$Camera1.make_current()
 	elif event.as_text() == "2":
-		$Chassis/Camera2.make_current()
+		$Camera2.make_current()
 
-func _physics_process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	
-	var suspension_dir: Vector3 = $Chassis/FrontSuspension.to_global($Chassis/FrontSuspension.curve.get_point_position(1) - $Chassis/FrontSuspension.curve.get_point_position(0)).normalized()
+	var move := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
-	var suspension_mount: Vector3 = $Chassis/FrontSuspension.to_global($Chassis/FrontSuspension.curve.get_point_position(1))
+	# move wheels based on speed
+	var angular_speed = velocity.dot(-global_basis.z) / wheel_mesh_radius * delta
+	$Mesh/FrontWheelSpring/Mesh.rotate_object_local(Vector3.RIGHT, angular_speed)
+	$Mesh/BackWheelSpring/Mesh.rotate_object_local(Vector3.RIGHT, angular_speed)
 	
-	var compress: Vector3 = $FrontWheel.global_position - $Chassis/FrontSuspension.to_global($Chassis/FrontSuspension.curve.get_point_position(0))
+	# lean based on acceleration
+	$Mesh.rotation.x = lerp_angle($Mesh.rotation.x, -move.y * pitch_intensity, 3.0 * delta)
+	$Mesh.rotation.z = lerp_angle($Mesh.rotation.z, -move.x * roll_intensity, 1.5 * delta)
 	
-	var compress_along_dir := suspension_dir.dot(compress)
-	compress -= suspension_dir * compress_along_dir
-	compress *= 2.0
-	compress += suspension_dir * compress_along_dir
+	# accelerate
+	velocity += global_basis.z * move.y * 10.0 * delta
 	
-	# stiffness (resist compression)
-	$Chassis.apply_force(compress * stiffness, suspension_mount - $Chassis.global_position)
-	$FrontWheel.apply_force(-compress * stiffness)
-	
-	# dampening (resist velocity)
-	$FrontWheel.apply_force(-($FrontWheel.linear_velocity - $Chassis.linear_velocity) * dampening)
-	
-	# wheel is a separate physical object from the bike itself
-	# normal/frictional forces act on the wheel, which applies normal/spring forces on the bike
-	# free body diagram
+	move_and_slide()
