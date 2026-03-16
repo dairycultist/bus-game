3585ff129ba50e72c1c40a595e44980c0c6f1c70
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-03-16 19:18:29 -0400

    Nice bike controller

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ A car sandbox game cuz I don't have internet
 https://x.com/TreyPowell1964/status/2030639705583206676?s=20
 
 explore the island of Santa Lucía
+
+mercy https://sketchfab.com/3d-models/overwatch-mercycasual-looks-cb8783e97efe455b9ff71246c9ac962d
--- car/bike.gd
+++ car/bike.gd
@@ -1,7 +1,19 @@
 extends CharacterBody3D
 
-# visual
+# less realistic controls (mario kart) > realistic
+
+@export_category("Handling")
+@export var max_speed: float = 40.0
+@export var acceleration: float = 20.0
+@export var max_turn_speed: float = 1.0
+@export var turn_acceleration: float = 5.0
+
+var speed: float
+var turn_speed: float
+
+@export_category("VFX")
 @export var pitch_intensity: float = 0.07
+@export var yaw_intensity: float = 0.3
 @export var roll_intensity: float = 0.5
 @export var wheel_mesh_radius: float = 1.2
 
@@ -14,18 +26,37 @@ func _input(event):
 
 func _process(delta: float) -> void:
 	
-	var move := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var move := -Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if move.y == 0.0:
+		move.y = -sign(speed)
+	
+	if move.x == 0.0:
+		move.x = -sign(turn_speed)
 	
 	# move wheels based on speed
 	var angular_speed = velocity.dot(-global_basis.z) / wheel_mesh_radius * delta
 	$Mesh/FrontWheelSpring/Mesh.rotate_object_local(Vector3.RIGHT, angular_speed)
 	$Mesh/BackWheelSpring/Mesh.rotate_object_local(Vector3.RIGHT, angular_speed)
 	
-	# lean based on acceleration
-	$Mesh.rotation.x = lerp_angle($Mesh.rotation.x, -move.y * pitch_intensity, 3.0 * delta)
-	$Mesh.rotation.z = lerp_angle($Mesh.rotation.z, -move.x * roll_intensity, 1.5 * delta)
-	
 	# accelerate
-	velocity += global_basis.z * move.y * 10.0 * delta
+	var accelerate_y = speed
 	
+	speed += move.y * acceleration * delta
+	speed = clamp(speed, -max_speed, max_speed)
+	velocity = -global_basis.z * speed
+	
+	accelerate_y = -sign(accelerate_y - speed)
+	
+	# turn
+	turn_speed += move.x * turn_acceleration * delta
+	turn_speed = clamp(turn_speed, -max_turn_speed, max_turn_speed)
+	global_rotation.y += turn_speed * delta
+	
+	# lean based on acceleration
+	$Mesh.rotation.x = lerp_angle($Mesh.rotation.x, accelerate_y * pitch_intensity, 3.0 * delta)
+	$Mesh.rotation.y = lerp_angle($Mesh.rotation.z, turn_speed * yaw_intensity, 1.5 * delta)
+	$Mesh.rotation.z = lerp_angle($Mesh.rotation.z, turn_speed * roll_intensity, 1.5 * delta)
+	
+	print(speed)
 	move_and_slide()
